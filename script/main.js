0e6efa66963cbb7ff8828e6e8d663218fbeb24dc
min0622 <151921634+min0622@users.noreply.github.com>
2024-08-16 13:12:51 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,32 +1,28 @@
 $(function () {
 
 
-    function color() {
+    $(window).resize(function () {
 
-        $('.link a').on({
-            mouseover: function () {
-                $(this).addClass('change');
-                $(this).css({border: '1px solid #DFFF9A'})
-            },
-            mouseout: function () {
-                $(this).removeClass('change');
-                $(this).css({border: '1px solid #fff'})
+        // let size = innerHeight();
+        let size =    $(window).innerWidth();
 
+        if (size > 767) {
 
-            }
-            // mouseover: function () {
-            //     $(this).css({backgroundColor:'#DFFF9A', color:'#000',border: '1px solid #DFFF9A'})
-            // },
-            // mouseout: function () {
-            //     $(this).css({backgroundColor:'#000', color:'#fff', border: '1px solid #fff'})
-            // }
+                $('.link a').on({
+                    mouseover: function () {
+                        $(this).addClass('change');
+                        $(this).css({ border: '1px solid #DFFF9A' })
+                    },
+                    mouseout: function () {
+                        $(this).removeClass('change');
+                        $(this).css({ border: '1px solid #fff' })
 
-        })
+                    }
 
-    }
-
-color();
-
-
-
+                })
+            
+        } 
+        
+    });
+    
 });
